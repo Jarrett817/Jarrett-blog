c7338eeabdddd71691998852048bcf5700da9638
wangjr <wangjr@getui.com>
2022-07-28 19:59:22 +0800

    ci: 集成naive ui && 测试按需引入及主题切换

---
+++ docs/.vitepress/config/link-config.ts
@@ -0,0 +1,41 @@
+const ComputerBasicSidebar = [
+  {
+    text: '设计模式',
+    link: '/computer-basic/design-pattern/'
+  },
+  {
+    text: '计算机网络',
+    link: '/computer-basic/internet/'
+  },
+  {
+    text: '数据结构与算法',
+    link: '/computer-basic/fucking-arithmetic/'
+  }
+];
+
+const FrontEndSidebar = [
+  {
+    text: 'HTML',
+    link: '/front-end/html/'
+  },
+  {
+    text: 'CSS',
+    link: '/front-end/css/'
+  },
+  { text: 'JS', link: '/front-end/js/' },
+  { text: 'TS', link: '/front-end/ts/' },
+  { text: '浏览器', link: '/front-end/browser/' },
+  { text: 'vue', link: '/front-end/vue/' },
+  { text: 'nodejs', link: '/front-end/nodejs/' },
+  { text: 'git', link: '/front-end/git/' },
+  { text: 'canvas', link: '/front-end/canvas/' }
+];
+
+const PackagesSidebar = [
+  {
+    text: 'ppt生成器',
+    link: '/packages/ppt/'
+  }
+];
+
+export { ComputerBasicSidebar, FrontEndSidebar, PackagesSidebar };
--- docs/.vitepress/theme/components/base-layout.vue
+++ docs/.vitepress/theme/components/base-layout.vue
@@ -1,26 +1,34 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme';
-import BackTop from './back-top.vue';
 import JSlides from './ppt/src/index.vue';
-import { ref, computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import { useData } from 'vitepress';
+import { darkTheme, lightTheme } from 'naive-ui';
 const { Layout } = DefaultTheme;
 const slidesVisible = ref(false);
 
-const actionList = computed(() => {
-  const icons = [{ icon: 'bofang', click: () => (slidesVisible.value = !slidesVisible.value) }];
-  if (!useData())
-    return [{ icon: 'exit', click: () => (slidesVisible.value = !slidesVisible.value) }];
-  if (useData().page.value.title === 'Home') return [];
-  return icons;
+const theme = ref<'dark' | 'light'>('light');
+
+const listenThemeChange = () => {
+  const buttonEl = document.querySelector('.VPSwitchAppearance');
+  buttonEl &&
+    buttonEl.addEventListener('click', () => {
+      theme.value = theme.value === 'dark' ? 'light' : 'dark';
+    });
+};
+
+onMounted(() => {
+  listenThemeChange();
 });
 </script>
 
 <template>
-  <j-slides v-if="slidesVisible" />
-  <Layout v-if="!slidesVisible">
-    <template #layout-bottom>
-      <!-- <back-top :actionList="actionList" /> -->
-    </template>
-  </Layout>
+  <n-config-provider :theme="theme === 'dark' ? darkTheme : lightTheme">
+    <j-slides v-if="slidesVisible" />
+    <Layout v-if="!slidesVisible">
+      <template #layout-bottom>
+        <n-back-top />
+      </template>
+    </Layout>
+  </n-config-provider>
 </template>
